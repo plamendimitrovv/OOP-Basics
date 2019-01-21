cd420568b54210ec49605cb58a0620e8c6f1545f
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-01-21 21:38:58 +0100

    Problem 4. Person Class

---
+++ Problem4.PersonClass/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Person
+{
+
+    string name;
+    int age;
+    List<BankAccount> accounts;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        this.accounts = new List<BankAccount>();
+    }
+
+    public Person(string name, int age, List<BankAccount> accounts): this (name,age)
+    {
+        this.name = name;
+        this.age = age;
+        this.accounts = accounts;
+    }
+
+    public decimal GetBalance()
+    {
+        return accounts.Sum(a => a.Balance);
+    }
+
+}
+
