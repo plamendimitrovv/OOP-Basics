217866c21a8746d92eeb89df0c4eda84af11ed07
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-01-27 16:13:01 +0100

    05. Date Modifier

---
+++ 5.Date Modifier/DataModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+
+public class DataModifier
+{
+    
+    public static int CalculateDateDifference(string firsDate, string secondDate)
+    {
+        var difference = DateTime.Parse(firsDate) - DateTime.Parse(secondDate);
+        return Math.Abs(difference.Days); 
+    }
+}
+
---
+++ 5.Date Modifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        string firstDate = Console.ReadLine();
+        string secondDate = Console.ReadLine();
+
+
+        Console.WriteLine(DataModifier.CalculateDateDifference(firstDate, secondDate)); 
+    }
+}
+
