44579636da83c2d1b6888adebe31b24bda19c51b
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-01-27 14:27:54 +0100

    Problem 3. Oldest Family Member

---
+++ Problem3.Oldest Family Member/Family.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Family
+{
+
+    public List<Person> people;
+    private bool isOrdered;
+
+    public Family()
+    {
+        people = new List<Person>();
+        isOrdered = false;    
+    }
+
+
+    public void AddMember(Person member)
+    {
+        people.Add(member);
+        isOrdered = false;
+
+    }
+
+    public Person GetOldestMember()
+    {
+        if (!isOrdered)
+        {
+            isOrdered = true;
+            return people.OrderByDescending(a => a.Age).FirstOrDefault(); 
+        }
+
+        return people[0];
+    }
+
+}
---
+++ Problem3.Oldest Family Member/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Family family = new Family();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string name = input[0];
+            int age = int.Parse(input[1]);
+
+            family.AddMember(new Person(name, age)); 
+        }
+
+
+        Person oldestPerson = family.GetOldestMember();
+
+        Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+    }
+}
+
+
