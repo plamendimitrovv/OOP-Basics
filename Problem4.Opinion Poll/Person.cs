4b70d07458f70c4173fa3a9da7d4022e51792fc2
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-01-27 15:08:03 +0100

    Problem 4. Opinion Poll

---
+++ Problem4.Opinion Poll/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+   public class Person
+    {
+        private string name;
+        private int age;
+
+    public Person()
+    {
+        this.Name =  "No name";
+        this.age = 1; 
+    }
+
+    public Person(int age):this()
+    {
+        this.Age = age;
+    }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
+
+    }
---
+++ Problem4.Opinion Poll/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            string name = input[0];
+            int age = int.Parse(input[1]);
+
+            Person person = new Person(name, age);
+
+            people.Add(person);
+        }
+
+        var sorted = people.Where(a => a.Age > 30).OrderBy(p => p.Name).ToList();
+
+        foreach (var person in sorted)
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+
+        
+    }
+}
+
