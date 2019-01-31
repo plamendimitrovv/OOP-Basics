f23a62aa3035f2807792780fcc278afe3716301d
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-01-31 23:51:28 +0100

    problem 6. company roster

---
+++ 6.Company Roster/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Employee
+{
+    //private string name;
+    //private decimal salary;
+    //private string position;
+    //private string email;
+    //private int age ;
+
+    public string Name { get; set; }
+
+    public decimal Salary { get; set; }
+
+    public string Position { get; set; }
+
+    public string Department { get; set; }
+
+    public string Email { get; set; }
+
+    public int Age { get; set; }
+
+
+    public Employee(string name, decimal salary, string position, string department)
+    {
+        this.Name = name;
+        this.Salary = salary;
+        this.Position = position;
+        this.Age = -1;
+        this.Email = "n/a";
+    }
+
+}
+
---
+++ 6.Company Roster/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> employees = new List<Employee>();
+
+        int peopleCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < peopleCount; i++)
+        {
+            string[] input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var employee = new Employee(input[0], decimal.Parse(input[1]), input[2], input[3]);
+
+            if (input.Length > 4)
+            {
+                var ageOrEmail = input[4];
+                if (ageOrEmail.Contains("@"))
+                {
+                    employee.Email = ageOrEmail;
+                }
+
+                else
+                {
+                    employee.Age = int.Parse(input[4]);
+                }
+            }
+
+            if (input.Length > 5)
+            {
+                employee.Age = int.Parse(input[5]);
+            }
+
+            employees.Add(employee);
+
+
+        }
+
+    }
+
+}
+
+
+ 
