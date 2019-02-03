11f40163a5dd793254a98d990eb7cee7038d29f2
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-02-03 21:31:19 +0100

    Problem 7.SpeedRacing

---
+++ 7.SpeedRacing/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Car
+{
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelConsumption { get; set; }
+    public double Distance { get; set; }
+
+    public Car(string model, double fuelAmount, double fuelConsumption)
+    {
+        this.Model = model;
+        this.FuelAmount = fuelAmount;
+        this.FuelConsumption = fuelConsumption;
+        this.Distance = 0;
+    }
+
+    public bool Drive(double distance)
+    {
+        double fuelNeeded = distance * this.FuelConsumption;
+        if (fuelNeeded > this.FuelAmount)
+        {
+            return false;
+        }
+
+        this.FuelAmount -= fuelNeeded;
+        this.Distance += distance;
+        return true;
+    }
+}
+
---
+++ 7.SpeedRacing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int carsToTrack = int.Parse(Console.ReadLine());
+        List<Car> cars = new List<Car>();
+
+        for (int i = 0; i < carsToTrack; i++)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            var modelCar = tokens[0];
+           // var fuelAmount = double.Parse(tokens[1]);
+           //  var fuelConsumption = double.Parse(tokens[2]);
+
+            if (!cars.Any(c => c.Model == modelCar))
+            {
+                Car car = new Car(modelCar, double.Parse(tokens[1]), double.Parse(tokens[2]));
+                cars.Add(car);
+            }
+        }
+
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            var tokens = input.Split();
+            string model = tokens[1];
+            double distance = double.Parse(tokens[2]);
+
+            Car car = cars.Find(c => c.Model == model);
+            bool isMoved = car.Drive(distance);
+
+            if (!isMoved)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        foreach (var c in cars)
+        {
+            Console.WriteLine($"{c.Model} {c.FuelAmount:F2} {c.Distance}");
+        }
+    }
+}
