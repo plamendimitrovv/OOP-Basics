1c5e046302e0dbbb8d551c85b34e4f94f0519d67
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-02-09 22:20:27 +0100

    Problem 8. Raw Data

---
+++ 8.RawData/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Engine
+{
+    // private int speed;
+    // private int power;
+
+    public int Speed { get; set; }
+    public int Power { get; set; }
+
+    public Engine(int speed, int power)
+    {
+        this.Speed = speed;
+        this.Power = power;
+    }
+
+}
---
+++ 8.RawData/Tire.cs
@@ -0,0 +1,26 @@
+﻿public class Tire
+{
+    private int age;
+    private double pressure;
+
+    public Tire(double pressure, int age)
+    {
+        this.Age = age;
+        this.Pressure = pressure;
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+
+    public double Pressure
+    {
+        get { return pressure; }
+        set { pressure = value; }
+    }
+
+}
+
