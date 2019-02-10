6736d5cf18c2712f10ffd9785d91c041a3c3bdd8
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-02-10 19:28:44 +0100

    problem 9. rectangle intersection

---
+++ 9.RectangleIntersection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] inputToken = Console.ReadLine().Split();
+        int numRec = int.Parse(inputToken[0]);
+        int numIntersect = int.Parse(inputToken[1]);
+        var rectangles = new List<Rectangle>();
+       
+
+        for (int i = 0; i < numRec; i++)
+        {
+            string[] RectanglesToken = Console.ReadLine().Split();
+            string id = RectanglesToken[0];
+            int width = int.Parse(RectanglesToken[1]);
+            int height = int.Parse(RectanglesToken[2]);
+            int coordinateX = int.Parse(RectanglesToken[3]);
+            int coordinateY = int.Parse(RectanglesToken[4]);
+
+            var rectangle = new Rectangle(id, width, height, coordinateX, coordinateY);
+            rectangles.Add(rectangle);
+        }
+
+        for (int i = 0; i < numIntersect; i++)
+        {
+            string[] pairs = Console.ReadLine().Split();
+            var firstRec = rectangles.FirstOrDefault(r => r.Id == pairs[0]);
+            var secondRec = rectangles.FirstOrDefault(r => r.Id == pairs[1]);
+            
+            Console.WriteLine(firstRec.IntersectsRectangle(secondRec) ? "true" : "false");
+        }
+
+
+    }
+}
---
+++ 9.RectangleIntersection/Rectangle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Rectangle
+{
+    private string id;
+    private int width;
+    private int height;
+    private int horizontal;
+    private int vertical;
+
+    public Rectangle(string id, int width, int height, int horizontal, int vertical)
+    {
+        this.id = id;
+        this.width = width;
+        this.height = height;
+        this.horizontal = horizontal;
+        this.vertical = vertical;
+    }
+
+    public string Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public int Width
+    {
+        get { return this.width; }
+        set { this.width = value; }
+    }
+
+    public int Height
+    {
+        get { return this.height; }
+        set { this.height = value; }
+    }
+
+    public int Horizontal
+    {
+        get { return this.horizontal; }
+        set { this.horizontal = value; }
+    }
+
+    public int Vertical
+    {
+        get { return this.vertical; }
+        set { this.vertical = value; }
+    }
+
+    public bool IntersectsRectangle(Rectangle r)
+    {
+        return this.ContainsRecCorner(r) || r.ContainsRecCorner(this);
+    }
+
+
+    private bool ContainsRecCorner(Rectangle r)
+    {
+        return this.ContainsPoint(r.horizontal, r.vertical) ||
+               this.ContainsPoint(r.horizontal, r.vertical + height) ||
+               this.ContainsPoint(r.horizontal + width, r.vertical) ||
+               this.ContainsPoint(r.horizontal + width, r.vertical + height);
+    }
+
+    private bool ContainsPoint(double x, double y)
+    {
+        return x >= this.horizontal
+            && x <= this.horizontal + width
+            && y >= this.vertical
+            && y <= this.vertical + height;
+    }
+}
