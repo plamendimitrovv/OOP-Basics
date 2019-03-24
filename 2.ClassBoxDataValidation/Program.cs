4f076dd06006c6e8210072318bc5818ef6a6f758
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-03-24 21:33:36 +0100

    Class box with data validation

---
+++ 2.ClassBoxDataValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double length = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        try
+        {
+
+            Box box = new Box(length, width, height);
+            double surfaceArea = box.SurfaceArea();
+            double lateralSurfaceArea = box.LateralSurfaceArea();
+            double volume = box.Volume();
+
+            Console.WriteLine($"Surface Area - {surfaceArea:F2}");
+            Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:F2}");
+            Console.WriteLine($"Volume - {volume:F2}");
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
+
