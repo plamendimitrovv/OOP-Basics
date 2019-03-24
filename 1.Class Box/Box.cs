cbf828ae48215bd0ac2d0f1f89f79544a6a876cc
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-03-24 20:57:09 +0100

    problem 1 class box finished

---
+++ 1.Class Box/Box.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public double Length
+    {
+        get { return length; }
+        set { length = value; }
+    }
+    
+    public double Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+
+    public double Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+
+
+    public Box(double length, double width, double height)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+    }
+
+    public double SurfaceArea()
+    {
+        var surfaceArea = (2*length*width)
+            + (2*length*height) 
+            + (2*width*height);
+        return surfaceArea;
+    }
+
+    public double LateralSurfaceArea()
+    {
+        var lateralSurfaceArea = (2 * length * height)
+                               + (2 * width * height);
+        return lateralSurfaceArea;
+    }
+
+    public double Volume ()
+    {
+        var volume = length * width * height;
+        return volume; 
+    }
+
+}
--- 1.Class Box/Program.cs
+++ 1.Class Box/Program.cs
@@ -5,6 +5,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        double length = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        Box box = new Box(length,width,height);
+        double surfaceArea = box.SurfaceArea();
+        double lateralSurfaceArea = box.LateralSurfaceArea();
+        double volume = box.Volume();
+
+        Console.WriteLine($"Surface Area - {surfaceArea:F2}");
+        Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:F2}");
+        Console.WriteLine($"Volume - {volume:F2}");
+
     }
 }
