f9ac73cc5290444da03d47dca307a8ffb677c738
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-04-07 20:30:37 +0200

    Inheritance

---
+++ 5.PizzaCalories/Dough.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Dough
+{
+    
+
+    private double weight;
+    private string flowerType;
+    private string backingTechnique;
+
+    public double Weight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+
+    public string FlowerType
+    {
+        get { return flowerType; }
+        set { flowerType = value; }
+    }
+
+
+    public string BackingTechnique
+    {
+        get { return backingTechnique; }
+        set { backingTechnique = value; }
+    }
+
+}
+
---
+++ 5.PizzaCalories/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Pizza
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public Dough Dough { get; set; } // testo
+    public List<Topping> Toppings { get; set; }
+}
+
