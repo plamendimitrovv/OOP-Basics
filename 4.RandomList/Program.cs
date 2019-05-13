dfec175ee0c1fd77248535dba566360871e732c8
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-05-13 21:52:42 +0200

    4. Random List

--- 4.RandomList/Program.cs
+++ 4.RandomList/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace _4.RandomList
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Hello World!");
     }
 }
+
