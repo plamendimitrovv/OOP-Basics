cfc03ececcf516ede92f1e09c7e1264218c63df0
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-06-09 13:48:22 +0200

    04. Telephony

---
+++ 4.Telephony/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] numbers = Console.ReadLine().Split();
+        string[] sites = Console.ReadLine().Split();
+
+        var smartphone = new Smartphone();
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(smartphone.Calling(number));
+        }
+
+        foreach (var site in sites)
+        {
+            Console.WriteLine(smartphone.Browsing(site));
+        }
+    }
+}
+
---
+++ 4.Telephony/Smartphone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Smartphone : ICallable, IBrowsable
+{
+    public Smartphone()
+    {
+    }
+
+    public string Browsing(string url)
+    {
+        if (url.Any(c => char.IsDigit(c)))
+        {
+            return "Invalid URL!";
+        }
+
+        else
+        {
+            return "Browsing: " + url + "!";
+        }
+    }
+
+    public string Calling(string number)
+    {
+        if (!number.Any(n => char.IsDigit(n)))
+        {
+            return "Invalid number!";
+        }
+        else
+        {
+            return "Calling... " + number;
+
+        }
+    }
+}
+
