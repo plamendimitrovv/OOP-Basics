9a51a9a3f9d27fbae9b05e2bcae0b83896a214ea
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-06-23 14:56:06 +0200

    6.Birthday Celebrations

---
+++ 6.BirthdayCelebrations/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Pet : IBirthable
+{
+    public Pet(string name, string birthday)
+    {
+        Name = name;
+        Birthdate = birthday;
+    }
+
+    public string Name { get; private set; }
+    public string Birthdate { get; set;}
+}
+
---
+++ 6.BirthdayCelebrations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var birthdayGuys = new List<IBirthable>();
+
+        string inputLine = Console.ReadLine();
+       
+
+        while (inputLine != "End")
+        {
+            var inputArgs = inputLine.Split().ToList();
+            string typeOfMember = inputArgs[0];
+            
+
+            IBirthable currentMember;
+
+            switch (typeOfMember)
+            {
+                case "Citizen":
+                    currentMember = new Citizen(inputArgs[1], int.Parse(inputArgs[2]), inputArgs[3], inputArgs[4]);
+                    birthdayGuys.Add(currentMember);
+                    inputArgs.RemoveAt(0);
+                    break;
+
+                case "Pet":
+                    currentMember = new Pet(inputArgs[1], inputArgs[2]);
+                    birthdayGuys.Add(currentMember);
+                    inputArgs.RemoveAt(0);
+                    break;
+                default:
+                    break;
+            }
+
+
+            inputLine = Console.ReadLine();
+            inputArgs.RemoveAt(0);
+        }
+
+        var inputYear = Console.ReadLine();
+
+        var filteredMembers = birthdayGuys.Where(m => m.Birthdate.EndsWith(inputYear)).ToArray();
+
+        foreach (var m in filteredMembers)
+        {
+            Console.WriteLine(m.Birthdate);
+        }
+
+    }
+}
+
